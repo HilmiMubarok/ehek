344255f6ffd925f7e98018658362da7d6a8c5272
HilmiMubarok <m.hilmimubarok@gmail.com>
2017-07-27 13:57:17 -0700

    Upload di Github

---
+++ class.php
@@ -0,0 +1,33 @@
+<?php
+
+//class mobil
+class Mobil
+{
+	public $warna = "hitam";
+	public $jenis = "baygon";
+	
+	function merk()
+	{
+		echo $this->jenis;
+	}
+}
+
+$mobil = new Mobil();
+echo $mobil->merk();
+echo "<html>";
+echo "<head>";
+echo "<title>";
+echo "</title>";
+echo "</head>";
+echo "<body>";
+echo "<table>";
+echo "<tr>";
+echo "<th>";
+echo "adasd";
+echo "</th>";
+echo "</tr>";
+echo "</table>";
+echo "</body>";
+echo "</html>";
+
+?>
